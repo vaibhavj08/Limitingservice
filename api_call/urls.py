6be2158a18fad4d4e1ddd34a8e1abc08bd0b8066
vaibhavj08 <joshivaibhav0508@gmail.com>
2020-07-17 13:42:41 +0530

    update files

--- api_call/urls.py
+++ api_call/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.login,name = 'login'),
     path('signup/', views.signup,name = 'signup'),
     path('api_call/', views.api_call,name = 'api_call'),
-    path('api_call/remaining_call/', views.remaining_call,name = 'remaining_call')
+    path('api_call/remaining_call/', views.remaining_call,name = 'remaining_call'),
+    path('api_call/logout/',views.logout,name = 'logout')
 ]
--- api_call/views.py
+++ api_call/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 
 # Create your views here.
-
+#function to signup
 def signup(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -17,6 +17,7 @@ def signup(request):
     else:
         return render(request,'home.html')
 
+#function to login
 def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -27,17 +28,18 @@ def login(request):
             auth.login(request,user)
             return redirect('/api_call')
         else:
-            return HttpResponse("<h1>403 Forbidden</h1>")
+            return HttpResponse("<h1>403 Forbidden</h1><h3>Wrong credential</h3>")
     else:
         return render(request,'login.html')
 
 
-one_minute = 60
 count = 0
 hour_request_count=300
 
-
+#function to call the api
 def api_call(request):
+    if request.user.is_anonymous:
+        return redirect('/')
     global count
 
     url = 'http://127.0.0.1:8000/get_number'
@@ -47,11 +49,17 @@ def api_call(request):
         print('count:',count)
 
     except requests.exceptions.ReadTimeout:
-        return render(request,'api.html',{'response':'403 Forbidden'})
+        return render(request,'api.html',{'response':'403 Call limit exausted for a minute'})
 
     return render(request,'api.html',{'response':response.text})
 
+#function to logout
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
+
+#function to check the remaining calls in an hour
 def remaining_call(request):
     r_call = hour_request_count-count
     return HttpResponse(r_call)
